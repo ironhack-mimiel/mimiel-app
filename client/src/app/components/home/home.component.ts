efa241abbc035cf27dbd113463763a27538be572
A-Tr <alvarotrancon@gmail.com>
2018-05-16 19:06:44 +0200

    maquetando, home lista, cards de las mieles

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SessionService } from '../../services/session.service';
 import { HoneyInfoService } from '../../services/honey-info.service';
-import { HoneyListComponent } from '../honey-list/honey-list.component';
+import { ActivatedRoute, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
@@ -9,14 +10,12 @@ import { HoneyListComponent } from '../honey-list/honey-list.component';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  title = "MI MIEL";
-  honeys: Array<object>;
-
-  constructor(public sessionService: SessionService) { }
+  honeys: Array<any>;
+  
+  constructor(public honeyService: HoneyInfoService, public router: Router) { }
 
   ngOnInit() {
-
+    this.honeyService.getAll().subscribe(honeys => this.honeys = honeys);
   }
-
 
 }
