d7a3acb76db133e173a906cc1906c482790148fe
A-Tr <alvarotrancon@gmail.com>
2018-05-17 20:20:47 +0200

    added hive cards

--- client/src/app/components/my-profile/my-profile-hives/my-profile-hives.component.ts
+++ client/src/app/components/my-profile/my-profile-hives/my-profile-hives.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SessionService } from '../../../services/session.service';
 import { HiveInfoService } from '../../../services/hive-info.service';
+import { Router } from '@angular/router';
 
 interface Hive {
   _id: string;
@@ -15,21 +16,17 @@ interface Hive {
   styleUrls: ['./my-profile-hives.component.css']
 })
 export class MyProfileHivesComponent implements OnInit {
-
   hives: Array<Hive>;
 
-
-
-  constructor(public sessionService: SessionService, public hiveService: HiveInfoService) {
-
-  }
+  constructor(
+    public sessionService: SessionService,
+    public hiveService: HiveInfoService,
+    public router: Router
+  ) {}
 
   ngOnInit() {
-    this.hiveService
-      .getAll(this.sessionService.user._id)
-      .subscribe(hives => {
-        this.hives = hives
-      });
+    this.hiveService.getAll(this.sessionService.user._id).subscribe(hives => {
+      this.hives = hives;
+    });
   }
-
 }
